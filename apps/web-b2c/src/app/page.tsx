c1e9d9ee7c52bda84a43c43a58acd4dd420d699b
André Loureiro <contato.loureiro1@gmail.com>
2024-08-19 19:42:08 -0300

    fix: build

--- apps/web-b2c/src/app/page.tsx
+++ apps/web-b2c/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <>
       <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-primary text-7xl font-semibold">B2B Firula</p>
+          <p>B2B Firula</p>
           <h1 className="mt-4 text-xl font-bold tracking-tight sm:text-5xl">
             Plataforma em construção
           </h1>
