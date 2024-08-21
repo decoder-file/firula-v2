6e15e9f6eb22e7f5d7a441230d8f7dbe96e2ed19
André Loureiro <contato.loureiro1@gmail.com>
2024-08-21 16:26:35 -0300

    feat: creating admin login screen

---
+++ apps/web-admin/src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p>Admin Firula</p>
+          <h1 className="mt-4 text-xl font-bold tracking-tight sm:text-5xl">
+            Plataforma em construção
+          </h1>
+          <p className="mt-6 text-base leading-7 ">
+            Estamos trabalhando para melhorar a plataforma. Em breve ela estará
+            disponível.
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
